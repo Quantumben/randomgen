5bf9bf88c6c617a8fe5b62396ea3e3aae0534fd2
QuantumBen <omosabenedict@gmail.com>
2025-05-03 06:47:11 -0700

    Created template and everything works

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import random
+from django.http import JsonResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'generator/index.html')
+
+def generate_random(request):
+    number = random.randint(1, 1000)
+    return JsonResponse({'number': number})
--- randomgen/urls.py
+++ randomgen/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from generator import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('generate_random', views.generate_random, name='generate'),# Add this line to include the index view
 ]
